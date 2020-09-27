a7e4044ad3407fb6ca1a78dee946228639e80da6
Vivica Cong Ma <vivicacongma@gmail.com>
2020-09-27 23:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let index = 0
+  for(let i = 0; i<array.length; i++){
+    if(array[i] < min){
+      min = array[i]
+      index = i
+    }
+  }return array.splice(index, 1)
 }
 
 function selectionSort(array){
-
+  let sorted = []
+  while(array.length){
+    let min = findMinAndRemove(array)
+    sorted.push(min)
+  }
+  return sorted
 }
